2e0afc49e844ece89f6836caf0c5aa43d10cff36
Abdiooa <abdiooa45@gmail.com>
2023-08-27 21:35:19 +0300

    added the distributed tracing

--- api-gateway-service/src/main/java/com/aoo/apigatewayservice/config/SecurityConfig.java
+++ api-gateway-service/src/main/java/com/aoo/apigatewayservice/config/SecurityConfig.java
@@ -2,14 +2,12 @@ package com.aoo.apigatewayservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
-import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
---
+++ product-service/src/main/java/com/aoo/productservice/config/ProductConfig.java
@@ -0,0 +1,7 @@
+package com.aoo.productservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProductConfig {
+}
